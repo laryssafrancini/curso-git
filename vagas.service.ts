4507aa968867d7fa48125108ee19edeeb41f62a1
Laryssa Francini <99519042+laryssafrancini@users.noreply.github.com>
2022-02-18 17:16:08 -0300

    Add files via upload

---
+++ vagas.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Vaga } from './models/Vagas.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class VagasService {
+private url = "http://localhost:3000/vagas";
+  constructor(private _httpClient: HttpClient) {  }
+getVagas(): Observable<Vaga[]>{
+  return this._httpClient.get<Vaga[]>(this.url);
+}
+}
