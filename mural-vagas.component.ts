841a324c389073cefbf951b8b323b9dbae401ebd
Laryssa Francini <99519042+laryssafrancini@users.noreply.github.com>
2022-02-18 17:07:12 -0300

    Add files via upload

---
+++ mural-vagas.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { Vaga } from '../models/Vagas.model';
+import { VagasService } from '../vagas.service';
+
+@Component({
+  selector: 'app-mural-vagas',
+  templateUrl: './mural-vagas.component.html',
+  styleUrls: ['./mural-vagas.component.css']
+})
+export class MuralVagasComponent implements OnInit {
+  public vagas: Vaga[] = [];
+  constructor(private _vagaService: VagasService) { }
+
+  ngOnInit(): void {
+    this.listarVagas();
+  }
+  listarVagas() {
+    this._vagaService.getVagas()
+      .subscribe(
+        retornaVaga => {
+          this.vagas = retornaVaga.map(
+            item => {
+              return new Vaga(
+                item.id,
+                item.nome,
+                item.foto,
+                item.descricao,
+                item.salario
+              );
+            }
+          )
+        }
+      )
+  }
+}
